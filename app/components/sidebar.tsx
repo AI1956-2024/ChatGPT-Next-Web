187da7e5c0a8a7b81be1bcab71e4feeee1cd114a
AI1956-2024 <ai163aiai@163.com>
2024-03-19 22:14:58 +0800

    升级前公告
    升级前公告

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          AI直接用 简单高效
+          AI分享 简单高效
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
